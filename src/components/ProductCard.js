189fc436b097a0e9d5aadc89e182c0fb79cee1d3
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-10 13:39:07 +0630

    create product page

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,3 +1,16 @@
-export const ProductCard = () => {
-  return <div className="productCard">Product Card</div>;
+import "./ProductCard.css";
+
+export const ProductCard = ({ product }) => {
+  const { name, price, image } = product;
+
+  return (
+    <div className="productCard">
+      <img src={image} alt={name} />
+      <p className="name">{name}</p>
+      <div className="action">
+        <p>${price}</p>
+        <button>Add To Cart</button>
+      </div>
+    </div>
+  );
 };
