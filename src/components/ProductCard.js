566978d8b1ee46036672d8933b9069074e98666e
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-11 16:49:27 +0630

    remove all features by 'context' and 'reducer'

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,23 +1,7 @@
-import { useState } from "react";
-import { useEffect } from "react";
-import { useCart } from "../context/CartContext";
 import "./ProductCard.css";
 
 export const ProductCard = ({ product }) => {
-  const { cartList, addToCart, removeFromCart } = useCart();
-  const [isInCart, setIsInCart] = useState(false);
-
-  const { id, name, price, image } = product;
-
-  useEffect(() => {
-    const productIsInCart = cartList.find((cartItem) => cartItem.id === id);
-
-    if (productIsInCart) {
-      setIsInCart(true);
-    } else {
-      setIsInCart(false);
-    }
-  }, [cartList, id]);
+  const { name, price, image } = product;
 
   return (
     <div className="productCard">
@@ -25,13 +9,7 @@ export const ProductCard = ({ product }) => {
       <p className="name">{name}</p>
       <div className="action">
         <p>${price}</p>
-        {isInCart ? (
-          <button className="remove" onClick={() => removeFromCart(product)}>
-            Remove
-          </button>
-        ) : (
-          <button onClick={() => addToCart(product)}>Add To Cart</button>
-        )}
+        <button>Add To Cart</button>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router } from "react-router-dom";
-import { CartProvider } from "./context/CartContext";
 import "./index.css";
 import App from "./App";
 
@@ -9,9 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Router>
-      <CartProvider>
-        <App />
-      </CartProvider>
+      <App />
     </Router>
   </React.StrictMode>
 );
