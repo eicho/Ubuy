d7a509df7082fbb58fb7e61f6a965a31b671513a
Ei Cho <32412506+eicho@users.noreply.github.com>
2023-03-11 18:09:39 +0630

    create add to cart features

--- src/store/cartSlice.js
+++ src/store/cartSlice.js
@@ -1,19 +1,19 @@
 const { createSlice } = require("@reduxjs/toolkit");
 
 const cartSlice = createSlice({
-    name: "cart",
-    initialState: {
-        cartList: [],
-        total: 0
+  name: "cart",
+  initialState: {
+    cartList: [],
+    total: 0,
+  },
+  reducers: {
+    add(state, action) {
+      const updatedCartList = state.cartList.concat(action.payload);
+      const total = state.total + action.payload.price;
+      return { ...state, total: total, cartList: updatedCartList };
     },
-    reducers: {
-        add(state, action){
-            console.log(action.payload);
-        },
-        remove(){
-
-        }
-    }
+    remove() {},
+  },
 });
 
 export const { add, remove } = cartSlice.actions;
